4c6c5c84930454245958d2cce71d8dfefc449b5f
Gaurav Kumar <gauravkumar@Gauravs-Mac-mini.local>
2026-02-20 17:00:43 +0530

    Use PORT environment variable

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=True)
